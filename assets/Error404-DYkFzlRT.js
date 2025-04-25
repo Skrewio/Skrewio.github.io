7989e9a9fa91d6058f519ca29597ad6b605f9454
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 21:44:23 +0200

    feat: add background icons

--- assets/Error404-DYkFzlRT.js
+++
@@ -1,1 +0,0 @@
-import{d as e,s as r}from"./index-CGZEnJNi.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
---
+++ assets/Error404-cNx9yzWe.js
@@ -0,0 +1,1 @@
+import{d as e,s as r}from"./index-CqPbC7vr.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
