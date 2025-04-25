0189cf325a56cfb3f84eb873102b8a3bcbcc2b89
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 02:47:25 +0200

    Fix drawer visibility

--- assets/Error404-6yVtJX4S.js
+++
@@ -1,1 +0,0 @@
-import{d as e,q as r}from"./index-DqISNogp.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
---
+++ assets/Error404-BD09LIkQ.js
@@ -0,0 +1,1 @@
+import{d as e,q as r}from"./index-DQMkzVJE.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
