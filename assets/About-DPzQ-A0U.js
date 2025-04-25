bf93136d221f83d645e53d3e6817b9f9c56e4c3d
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 02:25:43 +0200

    Initial commit

---
+++ assets/About-DPzQ-A0U.js
@@ -0,0 +1,1 @@
+import{u as t,t as a}from"./index-D9G5XxZh.js";var e=a("<div>");const s=()=>{const{t:o}=t();return e()};export{s as default};
---
+++ assets/Error404-BMruowAo.js
@@ -0,0 +1,1 @@
+import{d as e,q as r}from"./index-D9G5XxZh.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
