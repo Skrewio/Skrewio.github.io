ef54726854f190074c6f0a1f602d29608718dec9
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 21:35:45 +0200

    fix: fix translation

--- assets/Error404-BO4Ad69k.js
+++
@@ -1,1 +0,0 @@
-import{d as e,s as r}from"./index-nIw3dbYw.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
---
+++ assets/Error404-DYkFzlRT.js
@@ -0,0 +1,1 @@
+import{d as e,s as r}from"./index-CGZEnJNi.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
