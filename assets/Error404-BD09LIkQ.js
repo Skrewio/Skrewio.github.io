6d7c2610a5aeab003c135696344adf436225708f
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 03:25:25 +0200

    fix: fix back

--- assets/Error404-BD09LIkQ.js
+++
@@ -1,1 +0,0 @@
-import{d as e,q as r}from"./index-DQMkzVJE.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
---
+++ assets/Error404-DWl_1e0Z.js
@@ -0,0 +1,1 @@
+import{d as e,q as r}from"./index-DaGuRYCz.js";const o=()=>{const a=e();return r(()=>a("/"))};export{o as default};
