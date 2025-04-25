9f44de0e9fd7d239946b255e175d32f94ab2cad1
Florian Schaal <florian.schaal@protonmail.com>
2025-04-25 22:05:50 +0200

    feat: modify background icon color

--- assets/About-DtUPXcL1.js
+++
@@ -1,1 +0,0 @@
-import{u as i,t as o,b as t,i as s,f as c,h as d,j as l}from"./index-CqPbC7vr.js";var v="_1epdxkz1",m="_1epdxkz2",p="_1epdxkz3",u=o("<div><div></div><div>");const g=()=>{const{t:r}=i();return(()=>{var e=u(),a=e.firstChild,n=a.nextSibling;return t(e,v),t(a,m),s(a,()=>r("constructing")),t(n,p),s(n,c(l,{icon:d})),e})()};export{g as default};
